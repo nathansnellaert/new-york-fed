a2d096ab301307294bf36884f0d99f4a402e2f55
salmiakdrop <nathansnellaert@gmail.com>
2025-09-10 15:05:14 +0900

    ovo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-os.environ['CONNECTOR_NAME'] = 'new-york-fed'
+os.environ['CONNECTOR_NAME'] = 'nyf'
 os.environ['RUN_ID'] = os.getenv('RUN_ID', 'local-run')
 
 from utils import validate_environment, upload_data
@@ -17,28 +17,28 @@ def main():
     
     # Process and upload each asset immediately
     reference_rates_data = process_reference_rates()
-    upload_data(reference_rates_data, "reference_rates")
+    upload_data(reference_rates_data, "nyf_reference_rates")
     
     soma_holdings_data = process_soma_holdings()
-    upload_data(soma_holdings_data, "soma_holdings")
+    upload_data(soma_holdings_data, "nyf_soma_holdings")
     
     primary_dealer_data = process_primary_dealer_stats()
-    upload_data(primary_dealer_data, "primary_dealer_stats")
+    upload_data(primary_dealer_data, "nyf_primary_dealer_stats")
     
     ambs_data = process_ambs_operations()
-    upload_data(ambs_data, "ambs_operations")
+    upload_data(ambs_data, "nyf_ambs_operations")
     
     treasury_data = process_treasury_operations()
-    upload_data(treasury_data, "treasury_operations")
+    upload_data(treasury_data, "nyf_treasury_operations")
     
     seclending_data = process_securities_lending()
-    upload_data(seclending_data, "securities_lending")
+    upload_data(seclending_data, "nyf_securities_lending")
     
     fx_swaps_data = process_fx_swaps()
-    upload_data(fx_swaps_data, "fx_swaps")
+    upload_data(fx_swaps_data, "nyf_fx_swaps")
     
     repo_data = process_repo_operations()
-    upload_data(repo_data, "repo_operations")
+    upload_data(repo_data, "nyf_repo_operations")
 
 if __name__ == "__main__":
     main()
